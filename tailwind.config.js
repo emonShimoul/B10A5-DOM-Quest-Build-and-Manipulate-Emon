2953825d96fbb176eece7ad6ce758ab2a2e381bb
emonShimoul <emonshimoul@gmail.com>
2024-12-08 22:44:29 +0600

    completed the header

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        primaryColor: "#B4F461", // Example custom color
+        secondaryColor: "#F9F7F3",
+      },
+    },
+  },
+  plugins: [],
+};
